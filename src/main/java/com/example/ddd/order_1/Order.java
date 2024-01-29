d44d8bea61f6cb3b8741cf1235c4630297dde835
newhyodong <hyodg90@gmail.com>
2024-01-30 00:00:07 +0900

    Order 코드 리팩토링

--- src/main/java/com/example/ddd/order_1/Order.java
+++ src/main/java/com/example/ddd/order_1/Order.java
@@ -7,10 +7,22 @@ public class Order {
     private Money totalAmounts;
 
     public Order(List<OrderLine> orderLines) {
+        setOrderLines(orderLines);
+    }
+
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
         if(orderLines == null || orderLines.isEmpty()){
             throw new IllegalArgumentException("no OrderLine");
         }
-        this.orderLines = orderLines;
+    }
+
+    private void calculateTotalAmounts() {
         int sum = orderLines.stream()
                 .mapToInt(x -> x.getAmounts())
                 .sum();
